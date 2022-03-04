f1f0065bf8092d2a2a6b57dbd10198c254cfa2d4
João Geraldes <jsmgeraldes@gmail.com>
2022-03-04 21:13:44 +0000

    👶first

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import { Tier } from "./components/Tier";
+
+interface Handlers {
+  onClick: null | (() => void);
+}
+
+interface Tier {
+  price: number;
+  title: string;
+  subtitle: string;
+}
+
+interface ComponentData {
+  tiers: Tier[];
+  function: "onClick";
+}
+
+const mock: ComponentData = {
+  function: "onClick",
+  tiers: [
+    {
+      price: 100,
+      title: "Individual",
+      subtitle: "Subtitle #1",
+    },
+    {
+      price: 200,
+      title: "Corporations",
+      subtitle: "Subtitle #2",
+    },
+    {
+      price: 300,
+      title: "Organizations",
+      subtitle: "Subtitle #3",
+    },
+  ],
+};
+
+function App() {
+  function click() {
+    console.log("Clicked!");
+  }
+
+  const handlers: Handlers = {
+    onClick: click,
+  };
+
+  const handleClick = handlers[mock.function];
+
+  if (handleClick) {
+    handleClick();
+  }
+
+  return (
+    <div className="App">
+      <Tier />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tier.tsx
@@ -0,0 +1,5 @@
+import "./styles/tier.css";
+
+export function Tier() {
+  return <div className="tier-container">HEY!</div>;
+}
